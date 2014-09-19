aa9fec1e47c852efdb7178e528258127a07b4f1b
tm1990 <haavardpc@hotmail.com>
2014-09-19 17:57:31 +0200

    Added SendPacketToList and SendPacketToListExcept currently only used for current state of the chat program

--- src/main/java/MiscUtils/Network/PacketHandler.java
+++ src/main/java/MiscUtils/Network/PacketHandler.java
@@ -8,16 +8,41 @@ import cpw.mods.fml.relauncher.Side;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.network.Packet;
 
+import java.util.ArrayList;
 import java.util.EnumMap;
 
 public class PacketHandler {
 
 
 
-
     public static Packet GetPacket(AbstractPacket packet, EnumMap<Side, FMLEmbeddedChannel> channels){
         return channels.get(Side.SERVER).generatePacketFrom(packet);
     }
+
+
+    public static void SendPacketToPlayerListExcept(AbstractPacket packet, EnumMap<Side, FMLEmbeddedChannel> channels, ArrayList<EntityPlayer> list, EntityPlayer pla){
+
+        for(EntityPlayer pl : list) {
+
+            if(pl.getCommandSenderName().equalsIgnoreCase(pla.getCommandSenderName()))
+                continue;
+
+            channels.get(Side.SERVER).attr(FMLOutboundHandler.FML_MESSAGETARGET).set(FMLOutboundHandler.OutboundTarget.PLAYER);
+            channels.get(Side.SERVER).attr(FMLOutboundHandler.FML_MESSAGETARGETARGS).set(pl);
+            channels.get(Side.SERVER).writeAndFlush(packet);
+        }
+    }
+
+    public static void SendPacketToPlayerList(AbstractPacket packet, EnumMap<Side, FMLEmbeddedChannel> channels, ArrayList<EntityPlayer> list){
+
+        for(EntityPlayer pl : list) {
+
+            channels.get(Side.SERVER).attr(FMLOutboundHandler.FML_MESSAGETARGET).set(FMLOutboundHandler.OutboundTarget.PLAYER);
+            channels.get(Side.SERVER).attr(FMLOutboundHandler.FML_MESSAGETARGETARGS).set(pl);
+            channels.get(Side.SERVER).writeAndFlush(packet);
+        }
+    }
+
 
     public static void sendToAll(AbstractPacket packet, EnumMap<Side, FMLEmbeddedChannel> channels)
     {
@@ -54,6 +79,7 @@ public class PacketHandler {
 
     public static void sendToAllExcept(AbstractPacket packet, EntityPlayer player, EnumMap<Side, FMLEmbeddedChannel> channels)
     {
+        if(FMLCommonHandler.instance().getMinecraftServerInstance().getConfigurationManager().playerEntityList != null && FMLCommonHandler.instance().getMinecraftServerInstance().getConfigurationManager().playerEntityList.size() > 0)
         for(int i = 0; i < FMLCommonHandler.instance().getMinecraftServerInstance().getConfigurationManager().playerEntityList.size(); i++)
         {
             EntityPlayer player1 = (EntityPlayer)FMLCommonHandler.instance().getMinecraftServerInstance().getConfigurationManager().playerEntityList.get(i);
